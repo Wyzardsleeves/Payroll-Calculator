3a4842657e100d923cde077c36e039ba15f55939
Wyzardsleeves <jlattimor3web@gmail.com>
2025-08-03 21:12:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Coding Assessment for eBacon
+
+## Get it running
+
+Install it first
+```
+$ yarn install
+```
+
+Run the program
+```
+$ yarn start
+```
+
+Run the program for development
+```
+$ yarn start-dev
+```
+
+## Breakdown
+
+- Installed nodemon for development
+- Turned the jsonc into a jsonc
+  - The instructions mentioned that I can "reformat the meta data as needed" so I turned the jsonc into a json.
+  - I could have installed one of the jsonc packages that I found for npm but it seemed simpler to separate the instructions from the data and make it a json.
+  - In production I would have used whatever methods an procedures that I saw being employed in the codebase already.
+- I decided to make the caclculator modular and take in an array to map through rather than hard coding the employees by name in case you guys gave browny points for that.
+- Used [Moment](https://www.npmjs.com/package/moment) to get the difference between the start and end timestamps on the shifts.
+- I started from the "Math" and worked my way outwards from there.
+  - I iterated throught he punches for Mike first.
+  - After I got that to work I then iterated through the other Employees.
+- I made the output an Array.
+  - When it was just an Object it would be an [object Object].
+  - I probably could have invested time into correcting that but I imagined that it probably wasn't the important part of the assignment.
---
+++ index.js
@@ -0,0 +1,97 @@
+const moment = require('moment');
+const data = require('./mock_data/PunchLogicTest.json')
+
+// Get Job Data
+const jobMeta = data.jobMeta;
+
+const getWageAndBenefitRate = job => {
+  const findJob = jobMeta.find(site => site.job === job)
+  return findJob
+}
+
+const getShiftHours = ({job, start, end}) => {
+  const startTime = moment(start);
+  const endTime = moment(end);
+  const duration = moment.duration(endTime.diff(startTime));
+  const hours = {jobCalc: getWageAndBenefitRate(job), work_duration: duration.asHours()};
+  return hours;
+}
+
+const getWorkWithMultiplier = (work_duration, timeLog, rate) => {
+  let result = 0;
+  const {overtotal, doubleTotal} = timeLog
+
+  //overtime calcuator
+  if(overtotal > 0 && doubleTotal == 0){
+    const calcOt = work_duration - overtotal
+    result = (calcOt + (overtotal * 1.5)) * rate
+  }
+
+  // //doubletime calculator
+  if(doubleTotal > 0){
+    const calcDt = work_duration - doubleTotal
+    result = ((calcDt * 1.5) + (doubleTotal * 2)) * rate
+  }
+
+  return result
+}
+
+// Get Employee Data
+const employeeData = data.employeeData;
+
+//Sump up the time
+const sumUpPayroll = (employeeData) => {
+  const dataSheet = [];
+  
+  employeeData.map(employee => {
+    const timeLog = {
+      employee: employee.employee,
+      currentTotal: 0,
+      regularTotal: 0,
+      overtotal: 0,
+      doubleTotal: 0,
+      wageTotal: 0,
+      benefitTotal: 0
+    }
+    
+    const regularMax = Math.max(0, 40 - timeLog.currentTotal);
+    const overTimeMax = Math.max(0, 48 - timeLog.currentTotal - regularMax);
+
+    employee.timePunch.map(punch => {
+      const time = getShiftHours(punch, timeLog)
+      const {rate, benefitsRate} = time.jobCalc; //consolidate these two
+      const {work_duration} = time; // this one also
+
+      timeLog.currentTotal += work_duration;
+      timeLog.benefitTotal += (benefitsRate * work_duration)
+
+      if (timeLog.currentTotal <= regularMax) {
+        timeLog.regularTotal = timeLog.currentTotal;
+        timeLog.wageTotal += (rate * work_duration)
+      } else if (timeLog.currentTotal <= regularMax + overTimeMax) {
+        timeLog.regularTotal = regularMax;
+        timeLog.overtotal = timeLog.currentTotal - regularMax;
+        timeLog.wageTotal += getWorkWithMultiplier(work_duration, timeLog, rate)
+      } else {
+        timeLog.regularTotal = regularMax;
+        timeLog.overtotal = overTimeMax;
+        timeLog.doubleTotal = timeLog.currentTotal - regularMax - overTimeMax;
+        timeLog.wageTotal += getWorkWithMultiplier(work_duration, timeLog, rate)
+      }
+    })
+    const data = {
+      [timeLog.employee]: {
+        emlployee: timeLog.employee,
+        regular: timeLog.regularTotal.toFixed(4),
+        overtime: timeLog.overtotal.toFixed(4),
+        doubletime: timeLog.doubleTotal.toFixed(4),
+        wageTotal: timeLog.wageTotal.toFixed(4),
+        benefitTotal: timeLog.benefitTotal.toFixed(4)
+      }
+    }
+    dataSheet.push(data)
+  })
+  console.log(JSON.stringify(dataSheet, null, 2))
+}
+
+return(sumUpPayroll(employeeData))
